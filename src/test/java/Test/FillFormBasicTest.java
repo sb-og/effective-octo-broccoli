159b7dfcdb42b1c72cba233c9795f8b86f16057d
szymon.bogus <szymon.bogus@asseco.pl>
2024-03-14 10:58:43 +0100

    test

--- src/test/java/Test/FillFormBasicTest.java
+++ src/test/java/Test/FillFormBasicTest.java
@@ -1,6 +1,11 @@
 package Test;
+import org.junit.jupiter.api.Test;
 
 public class FillFormBasicTest
 {
-
+    @Test
+    void FillFormBasicScenario()
+    {
+        System.out.println("selenium");
+    }
 }
